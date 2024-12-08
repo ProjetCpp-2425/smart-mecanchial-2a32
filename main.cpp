aa44e7f659c6e2ce314dc1aac5cf16ffada3588d
zeine <zeineb.elabed@esprit.tn>
2024-12-08 22:48:59 +0100

    First commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <QApplication>
+#include <QMessageBox>
+#include <QDebug>
+#include "connexion.h"
+#include "mainwindow.h"
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    connexion c;
+    bool test = c.CreateConnexion();
+
+    if (test) {
+        QMessageBox::information(nullptr, QObject::tr("Database Connection"),
+                                 QObject::tr("Connection successful.\nClick Cancel to exit."),
+                                 QMessageBox::Cancel);
+    } else {
+        QMessageBox::critical(nullptr, QObject::tr("Database Connection"),
+                              QObject::tr("Connection failed.\nClick Cancel to exit."),
+                              QMessageBox::Cancel);
+    }
+
+    MainWindow w;
+    w.show();
+
+    int result = a.exec();
+    c.FermerConnexion(); // Close the connection when the application exits
+
+    return result;
+}
